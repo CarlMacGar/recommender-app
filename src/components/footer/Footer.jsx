c270b25d332432ae4a804953f00cfa81cb73d16b
CarlMacGar <carlosgsolarte@gmail.com>
2025-05-24 21:21:07 -0500

    Just

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () =>{
     return(
         <footer className="py-4 bg-primary-dark text-sec-txt-light">
             <div className="container mx-auto text-center text-sm md:text-lg">
-                <p>&copy; {currentYear} - Developed by <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/CarlMacGar" target="_blank">CarlMacGar</a> - <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/Jaycom17" target="_blank">Jaycom17</a> - <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/FranSotelo2303" target="_blank">FranSotelo2303</a></p>
+                <p>&copy; {currentYear} - Developed by <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/CarlMacGar" target="_blank">CarlMacGar</a> - <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/Jaycom17" target="_blank">Jaycom17</a> - <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/FranSotelo2303" target="_blank">FranSotelo2303</a> - <a className="duration-150 text-vibrant-blue-light hover:text-aqua-dark" href="https://github.com/Guillermo1298" target="_blank">Guillermo1298</a></p>
                 <p>All rights reserved.</p>
             </div>
         </footer>
